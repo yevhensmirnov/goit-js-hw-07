8a516331b92bb8c36ca0c2498d175b9256c36219
yevhensmirnov <76063173+yevhensmirnov@users.noreply.github.com>
2021-05-25 01:43:06 +0300

    1

--- js/task-06.js
+++ js/task-06.js
@@ -5,15 +5,11 @@ let numbInputLength = parseInt(inputLength);
 input.addEventListener('input', onInutChange);
 function onInutChange(e) {
 
-    if (e.currentTarget.value.length <= numbInputLength) {
+    if (e.currentTarget.value.length === numbInputLength) {
         input.classList.remove("invalid");
         input.classList.add("valid");
 
-    } if (e.currentTarget.value.length === 0) {
-        input.classList.remove("invalid");
-        input.classList.remove("valid");
-
-    } if (e.currentTarget.value.length > numbInputLength) {
+    }  if (e.currentTarget.value.length !== numbInputLength) {
         input.classList.add("invalid");
         input.classList.remove("valid");
     }
