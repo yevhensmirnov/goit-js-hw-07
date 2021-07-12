05e91e53b86d4462e3a23536574f289e88c227ab
yevhensmirnov <76063173+yevhensmirnov@users.noreply.github.com>
2021-07-12 20:11:12 +0300

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,9 +1,9 @@
 const category = document.querySelectorAll(".item");
 console.log(`В списке ${category.length} категории.`);
 
-const categoriyList = [...category].map(
-    categories => `Категория: ${categories.children[0].textContent}
-Количество элементов: ${categories.children[1].children.length}`
-)
-    .join("\n");
-console.log(categoriyList);
+
+
+for (let el of category) {
+    console.log(`Категория: ${el.children[0].textContent}`);
+    console.log(`Количество элементов: ${el.children[1].children.length}`);
+  }
